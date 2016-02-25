04b6769a680bdffcccb59acf05241dae3cc3c9a9
mominirfan <Momin.Irfan@live.com>
2016-02-25 14:38:49 -0600

    rid of the random commmets

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,11 @@ import rxtxrobot.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		//will it track this cahmge
-		//hi 
+		
 		RXTXRobot r = new ArduinoUno(); 
 		r.setVerbose(true);
 		r.connect();
-		//newest test
-		//newest change
 		r.close();
-
-		//lasfdlsadfiwoafjoasj
-		//dteresa
-
-// new change here
-		//catcatcat
-		//yo
 
 	}
 
