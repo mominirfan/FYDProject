46d6eee941f84f0cef530a96d6dcc245aca9a59a
teresa201 <tyienger@smu.edu>
2016-02-28 17:26:42 -0600

    added code to move a servo

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,17 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		r = new ArduinoUno(); 
-		r.setPort("COM3");
+		r.setPort("/dev/tty.usbmodem1421");
+		//r.setVerbose(true); // Turn on debugging messages 
 		r.connect();
+		r.attachServo(RXTXRobot.SERVO1, 9);
+		r.moveServo(RXTXRobot.SERVO1, 30); // Move Servo 1 to location 30 
 
 	    //Get the average thermistor reading
-	    int thermistorReading = getThermistorReading();
+	    double thermistorReading = getThermistorReading();
 	    
 	    System.out.println("The probe read the value: " + thermistorReading);
-	    System.out.println("In volts: " + (thermistorReading * (5.0/1023.0)));
+	   System.out.println("In volts: " + (thermistorReading * (5.0/1023.0)));
 	    
 		r.close();
 	}
@@ -22,17 +25,17 @@ public class Main {
 		takes the average, then returns the result.
 		@return the average of the 10 results 
 	*/ 
-	public static int getThermistorReading() 
+	public static double getThermistorReading() 
 	{
-		int sum = 0;
-		int readingCount = 10;
+		double sum = 0;
+		double readingCount = 10;
 			    
 		//Read the analog pin values ten times, adding to sum each time
 		for (int i = 0; i < readingCount; i++) 
 		{
 			//Refresh the analog pins so we get new readings
 			r.refreshAnalogPins();
-			int reading = r.getAnalogPin(0).getValue();
+			double reading = r.getAnalogPin(1).getValue();
 			sum += reading;
 		}
 		
